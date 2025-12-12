3301154a1ac99937171a0935782940d16e41c193
Firdaun <ketuakls776@gmail.com>
2025-12-12 09:48:25 +0700

    delete mx-4 in ProtectedAbout.jsx

--- frontend/src/components/ProtectedAbout.jsx
+++ frontend/src/components/ProtectedAbout.jsx
@@ -87,7 +87,7 @@ export default function ProtectedAbout() {
                     <form
                         onSubmit={handleSubmit}
                         onClick={(e) => e.stopPropagation()}
-                        className="bg-white relative w-11/12 sm:max-w-lg md:max-w-2xl p-6 md:p-10 md:shadow-xl rounded-2xl md:border border-blue-200 transform transition-all duration-500 mx-4">
+                        className="bg-white relative w-11/12 sm:max-w-lg md:max-w-2xl p-6 md:p-10 md:shadow-xl rounded-2xl md:border border-blue-200 transform transition-all duration-500">
                         <div onClick={handleClose} className="active:scale-110 text-red-500 absolute right-3 top-3 cursor-pointer transition-transform">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
                         </div>
